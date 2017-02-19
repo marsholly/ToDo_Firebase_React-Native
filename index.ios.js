4dc429ccb00f0634f31f7916590fc9a7f24bc1bf
HONG ZHOU <holly_zhou@yahoo.com>
2017-02-19 16:36:46 -0700

    test firebase

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TouchableHighlight, TextInput, ListView } from 'react-native';
+import firebase from 'firebase';
 
 export default class devdacticFirebase extends Component {
+  constructor(props) {
+    super(props);
+    let config = {
+      
+    };
+    firebase.initializeApp(config);
+    let myFireBaseRef = firebase.database().ref();
+    myFireBaseRef.set({
+      title: 'Hello world!',
+      author: 'Holly'
+    });
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
 
 AppRegistry.registerComponent('devdacticFirebase', () => devdacticFirebase);
